65ea33377d5db9f4a590e897335a1435791513b1
thenatural86 <philip.smalls86@gmail.com>
2020-02-21 10:01:01 -0500

    fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,10 @@ import jsonPlaceholder from "../apis/jsonPlaceholder"
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts")
-  dispatch({
-    type: "FETCH_POSTS",
-    payload: response.data
-  })
+  dispatch({ type: "FETCH_POSTS", payload: response.data })
+}
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`)
+  dispatch({ type: "FETCH_USER", property: response.data })
 }
