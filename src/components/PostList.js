798570e0c3ce58a7374878b14d04a12f75d40d09
thenatural86 <philip.smalls86@gmail.com>
2020-02-21 08:48:30 -0500

    response.data in action

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,8 +8,15 @@ class PostList extends Component {
   }
 
   render() {
+    console.log(this.props.posts)
     return <div>PostList</div>
   }
 }
 
-export default connect(null, { fetchPosts })(PostList)
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList)
