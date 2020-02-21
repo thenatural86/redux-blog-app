ee409264a87f92f8cd87cab016515a3e6795ecda
thenatural86 <philip.smalls86@gmail.com>
2020-02-21 08:39:48 -0500

    switch case in reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,3 +1,8 @@
-export default (state, action) => {
-  return {}
+export default (state = [], action) => {
+  switch (action.type) {
+    case "FETCH_POSTS":
+      return action.payload
+    default:
+      return state
+  }
 }
