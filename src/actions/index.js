9bfbd4fdd6cd99f6311496f610d93b4427cb6d4c
thenatural86 <philip.smalls86@gmail.com>
2020-02-21 07:32:48 -0500

    reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
-export const fetchPosts = () => {
-  return {
-    type: "FETCH_POST"
-  }
+import jsonPlaceholder from "../apis/jsonPlaceholder"
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts")
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response
+  })
 }
